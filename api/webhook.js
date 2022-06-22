2608639743c92d092d94399af61a21a552213e18
Andrew Nosov <andrewnosov@outlook.com>
2022-06-22 09:21:50 +0500

    ADD test allow origin url

--- api/webhook.js
+++ api/webhook.js
@@ -4,7 +4,7 @@ import TelegramBot from 'node-telegram-bot-api';
 const allowCors = (fn) => async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
     // replace * with app url
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://eighties.vercel.app/');
     res.setHeader(
         'Access-Control-Allow-Methods',
         'GET,OPTIONS,PATCH,DELETE,POST,PUT'
