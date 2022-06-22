3b040bddcf71d99419bf002a77f17e0f071e4a5d
Andrew Nosov <andrewnosov@outlook.com>
2022-06-22 09:10:47 +0500

    ADD cors wrapper right in webhook code

--- api/webhook.js
+++ api/webhook.js
@@ -1,7 +1,27 @@
 import TelegramBot from 'node-telegram-bot-api';
 
+// enable CORS in a single serverless func (wrapper)
+const allowCors = (fn) => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // replace * with app url
+    res.setHeader('Access-Control-Allow-Origin', process.env.APP_URL);
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+    return await fn(req, res);
+};
+
 // bot send msg handler
-export default async (req, res) => {
+const handler = async (req, res) => {
     try {
         const bot = new TelegramBot(process.env.BOT_TOKEN); // create telegram bot handler
 
@@ -24,3 +44,6 @@ export default async (req, res) => {
     // send a 200 HTTP status code
     res.status(200).send('Success!');
 };
+
+// export secured by CORS handler
+export default allowCors(handler);
